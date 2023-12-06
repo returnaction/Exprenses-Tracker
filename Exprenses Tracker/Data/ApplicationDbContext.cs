9c6c84a65af98616ca06c8c5f2c9483521330e8b
returnaction <obergannv@gmail.com>
2023-12-05 23:59:31 -0500

    Add project files.

---
+++ Exprenses Tracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Exprenses_Tracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Exprenses Tracker/Data/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exprenses_Tracker.Data
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        // nav props
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
